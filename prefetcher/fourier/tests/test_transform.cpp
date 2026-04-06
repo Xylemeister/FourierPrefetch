722dc6a45d32da1bf48ac6829cb641c2ca610e79
Hanif H Rais <hhr22@login-ai.cx3.hpc.ic.ac.uk>
2026-04-06 18:57:18 +0100

    setup environment for testing using gtest and first test case

---
+++ prefetcher/fourier/tests/test_transform.cpp
@@ -0,0 +1,28 @@
+
+#include <cstdint>
+#include <vector>
+#include <gtest/gtest.h>
+#include "../transform.h"
+
+
+// --- Test the buffer ----
+using namespace Transform;
+
+
+TEST(Buffer, BufferTest){
+    std::vector<uint64_t> expected = {420, 69, 0, 67};
+
+    auto* fut = new TransformBuf<3>;
+
+    for (auto a: expected){
+        fut->Insert(a);
+    }
+
+    const uint64_t* it = fut->viewBuf();
+
+    EXPECT_EQ(it[0], 67);
+    EXPECT_EQ(it[1], 69);
+    EXPECT_EQ(it[2], 0);
+
+    delete fut;
+}
